bd833848553541348ac4a22c664b982b6307ba25
manuzcheruz <kipkemoi.emanuel@gmail.com>
2021-01-14 14:18:35 +0300

    done with testimonials

--- src/components/LandingPage/Testimonials/Testimonials.js
+++ src/components/LandingPage/Testimonials/Testimonials.js
@@ -1,9 +1,30 @@
 import React from 'react'
 
+import './Testimonials.css'
+import TestimonialsCard from './TestimonialsCard/TestimonialsCard'
+
 function Testimonials() {
     return (
-        <div>
-            Testimonials
+        <div className="section" style={{backgroundColor: 'white'}}>
+            <div className="container">
+                <div className="testimonial-wrapper">
+                    <div className="title">
+                        What <span>Jobholders</span> say 
+                        <br />
+                        about us
+                    </div>
+                    <div className="testimonials">
+                        <TestimonialsCard />
+                        <TestimonialsCard />
+                        <TestimonialsCard />
+                        <TestimonialsCard />
+                        <TestimonialsCard />
+                        <TestimonialsCard />
+                        <TestimonialsCard />
+                        <TestimonialsCard />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
